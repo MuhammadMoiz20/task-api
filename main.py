e13d3577e3880f0998a8c6f0d9d8c69903c17110
Muhammad Moiz <moizzahid20@gmail.com>
2023-06-11 10:00:00 -0400

    feat: add CRUD endpoints

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-from fastapi import FastAPI
-app = FastAPI(title='Task API')
+tasks = []
+@app.get('/tasks', response_model=list[Task])
+def get_tasks():
+    return tasks
+@app.post('/tasks', response_model=Task)
+def create_task(task: TaskCreate):
+    task_id = len(tasks) + 1
+    new_task = Task(id=task_id, title=task.title)
+    tasks.append(new_task)
+    return new_task

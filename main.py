2e97038d6418972b74780a5b5b71df4f4a7d4155
Muhammad Moiz <moizzahid20@gmail.com>
2023-06-12 10:00:00 -0400

    feat: add update endpoint

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def create_task(task: TaskCreate):
     new_task = Task(id=task_id, title=task.title)
     tasks.append(new_task)
     return new_task
+@app.put('/tasks/{task_id}', response_model=Task)
+def update_task(task_id: int, task: TaskCreate):
+    for t in tasks:
+        if t.id == task_id:
+            t.title = task.title
+            return t
+    raise HTTPException(status_code=404, detail='Task not found')
